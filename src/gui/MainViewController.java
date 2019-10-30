e1795c3d13a73d2d2de70a20260761c6bedb52d6
miltonferreira <jotonferreira@gmail.com>
2019-10-30 09:42:21 -0300

    Mostrando DepartmentList ao clicar no bt Department do menu

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -36,7 +36,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void OnMenuItemDepartmentAction() {
-		System.out.println("OnMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml"); // carrega a tela com lista de departamentos
 	}
 	
 	@FXML
