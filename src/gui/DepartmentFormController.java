608ac6821f409ec953d57a35bc375855c036d08e
miltonferreira <jotonferreira@gmail.com>
2019-11-01 05:45:11 -0300

    Criado Controlador do DepartmentForm.fxml

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,55 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable{
+	// controla janela do DepartmentForm.fxml <<<<<<<<<<<<< 
+
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	// Métodos para tratar eventos dos TextField's, Label e Button's ----------------
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+	
+	// restricoes dos TextField's, Label
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId); // aceita somente inteiros
+		Constraints.setTextFieldMaxLength(txtName, 30); // aceita somente 30 caracteres no texto
+	}
+
+}
